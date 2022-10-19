78a7afea2e194a7284483673d3b5765cb77c9079
Leo Parra <sparra2398@gmail.com>
2022-10-19 13:29:03 -0700

    added comments for readability

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//Ready sets up our function to run only after document page has loaded first
 $(document).ready(function (){
 
+    //Creates the date at the top of the page
     document.getElementById("currentDay").textContent = moment().format('dddd, MMMM Do YYYY');
+    //Allows the color of the rows to change based on hour of the day
     var currentHour = moment().hours()
-    
     for (var i = 8; i < 19; i++){
         var colorKey = ""
         if (i < currentHour){
@@ -14,7 +16,8 @@ $(document).ready(function (){
         else {
             colorKey = "future"
         }
-        
+
+        //Changes the hour display in left column from 24-hour clock to 12-hour clock with AMs and PMs
         var hourDisplay = "";
         if (i < 12){
             hourDisplay = i + "am"
@@ -26,6 +29,7 @@ $(document).ready(function (){
             hourDisplay = i - 12 + "pm"
         }
 
+        //Creates grid system with hour, text column and save button in html using jQuery
         var rowDiv = $("<div>").addClass("row").attr("id", i);
         var hourDiv = $("<div>").addClass("col-1").text(hourDisplay);
         var textArea = $("<textarea>").addClass("col-8 description " + colorKey).val(localStorage.getItem(i))
